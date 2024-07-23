c33dd210a0c9c16579302a75c8e4132bde3b6a9f
David Reed <reedswork@proton.me>
2024-07-23 22:39:24 +0300

    add sick nasty landing page

---
+++ README.md
@@ -0,0 +1,5 @@
+# Holy Unblocker: Lander
+
+<img src="./public/holey.png" width="128" height="128">
+
+this is a silly little landing page
---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import node from "@astrojs/node";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+});
